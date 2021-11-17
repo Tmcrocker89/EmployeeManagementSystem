65c877fbab6ce7d8a005419a2af0f386e07f862c
Travis Crocker <Travis.Crocker@outlook.com>
2021-11-16 19:12:48 -0500

    Finishing tocuhes

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql2");
-const addRole = require("./lib/AddRole")
-const addEmp = require("./lib/AddEmployee")
 const manageEmp = require("./lib/ManageEmp");
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-  );
-
-
-
-
-manageEmp()
+manageEmp();
 
 
 
